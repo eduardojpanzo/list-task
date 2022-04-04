e1d5db32da842fdda01e6f6cdd012c7b2a24aa79
João Eduardo Panzo <john404edwards@gmail.com>
2022-04-04 22:31:07 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,25 @@
+let userActive;
+
+function enableUser(){
+    userActive = users[1];
+}
+
+
 //Validar a Entrada no password....
 function handleUser(user) {
+    document.querySelector('.home .user-img img').src = `${user.urlImg}`;
+    document.querySelector('.home .user--name').innerHTML = `${user.name}`;
+    
+}
+
+function confirmUser(user) {
     if(passwordComfirmed(user.password)){
         handleUserList(user)
     }
 }
 
-function passwordComfirmed(password) {
-    
+function passwordComfirmed(user,password) {
+    return user.password === password;
 }
 //A partir do login ir na segundo pagina 'lists'
 //Carregar enumeras tarefas apartir do usuario escolhido
