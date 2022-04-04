1346ebf6c039cc06270ba9c6cd4902f533bd5633
João Eduardo Panzo <john404edwards@gmail.com>
2022-04-04 21:07:29 +0100

    criação do default.js
    informação padrão nos dados

---
+++ assets/js/default.js
@@ -0,0 +1,11 @@
+const users = [
+    {id:1,name:'Jordam Machael',password:'JM1'},
+    {id:2,name:'Miguel Eduardo',password:'ME2'},
+    {id:3,name:'Pedro Anfré',password:'PA3'},
+    {id:4,name:'Afonso Mateus',password:'AM4'},
+    {id:5,name:'Carolina Maria',password:'CM5'}
+];
+
+const listTask = [
+    {id:1, title:''}
+]
