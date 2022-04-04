02aa164806d5775fadca414f3ae424bc5a859f8e
João Eduardo Panzo <john404edwards@gmail.com>
2022-04-04 21:24:00 +0100

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -7,5 +7,61 @@ const users = [
 ];
 
 const listTask = [
-    {id:1, title:''}
+    {
+        id:1,
+        userRef:1,
+        title:'Fazer Compras',
+        tasks:[
+            {id:1,name:"Arroz"},
+            {id:2,name:"Feijão"},
+            {id:3,name:"Tomate"},
+            {id:4,name:"Farinha"}
+        ]
+    },
+    {
+        id:2,
+        userRef:1,
+        title:'Programar',
+        tasks:[
+            {id:1,name:"Java"},
+            {id:2,name:"JavaScript"},
+            {id:3,name:"Php"}
+        ]
+    },
+    {
+        id:3,
+        userRef:2,
+        title:'Estudar Física',
+        tasks:[
+            {id:1,name:"Mecânica"},
+            {id:2,name:"Elétrostatica"},
+            {id:3,name:"Termodinâmica"},
+            {id:4,name:"Ondas"}
+        ]
+    },
+    {
+        id:4,
+        userRef:2,
+        title:'Fazer Exercícos',
+        tasks:[
+            {id:1,name:"Agachamento"},
+            {id:2,name:"Fleções"},
+            {id:3,name:"Levantar Peso"},
+            {id:4,name:"Correr"},
+            {id:5,name:"Saltar"}
+        ]
+    },
+    {
+        id:5,
+        userRef:3,
+        title:'Fazer Compras',
+        tasks:[
+            {id:1,name:"PC"},
+            {id:2,name:"Impressora"},
+            {id:3,name:"Tinta"},
+            {id:4,name:"Lapiseras"},
+            {id:4,name:"Cadernos"},
+            {id:5,name:"Roupas"}
+        ]
+    }
 ]
