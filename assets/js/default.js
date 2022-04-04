44133c99ed248bd3a2cdf45ea9573714ce813e6e
João Eduardo Panzo <john404edwards@gmail.com>
2022-04-04 22:31:22 +0100

    Update default.js

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,9 +1,9 @@
 const users = [
-    {id:1,name:'Jordam Machael',password:'JM1'},
-    {id:2,name:'Miguel Eduardo',password:'ME2'},
-    {id:3,name:'Pedro Anfré',password:'PA3'},
-    {id:4,name:'Afonso Mateus',password:'AM4'},
-    {id:5,name:'Carolina Maria',password:'CM5'}
+    {id:1,name:'Jordam Machael',password:'JM1',urlImg:'../../media/homem.png'},
+    {id:2,name:'Miguel Eduardo',password:'ME2',urlImg:'../../media/homem1.png'},
+    {id:3,name:'Pedro Anfré',password:'PA3',urlImg:'../../media/homem2.png'},
+    {id:4,name:'Afonso Mateus',password:'AM4',urlImg:'../../media/mulher1.png'},
+    {id:5,name:'Carolina Maria',password:'CM5',urlImg:'../../media/mulher2.png'}
 ];
 
 const listTask = [
