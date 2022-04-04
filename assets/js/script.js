300cf7c59f49d7f8cfaa1287c18acd208eac4b97
João Eduardo Panzo <john404edwards@gmail.com>
2022-04-04 21:07:50 +0100

    Create script.js
    lógica do App

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+//Validar a Entrada no password....
+//A partir do login ir na segundo pagina 'lists'
+//Carregar enumeras tarefas apartir do usuario escolhido
+//Da tarefa escolhida ter os feitos e não feiots
