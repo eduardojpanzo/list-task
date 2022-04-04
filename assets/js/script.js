63d133a054affacca9f5e3d458dfe20180e3d0cb
João Eduardo Panzo <john404edwards@gmail.com>
2022-04-04 21:46:16 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,13 @@
 //Validar a Entrada no password....
+function handleUser(user) {
+    if(passwordComfirmed(user.password)){
+        handleUserList(user)
+    }
+}
+
+function passwordComfirmed(password) {
+    
+}
 //A partir do login ir na segundo pagina 'lists'
 //Carregar enumeras tarefas apartir do usuario escolhido
 //Da tarefa escolhida ter os feitos e não feiots
