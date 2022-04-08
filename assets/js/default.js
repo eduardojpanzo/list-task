e67d5c5e9d62897a4e3a38517c98dc46e7277bb4
João Eduardo Panzo <john404edwards@gmail.com>
2022-04-08 09:56:36 +0100

    mudanças e factoração

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,12 +1,16 @@
 const select = document.querySelector('#accountType');
-const home = document.querySelector('.home.container')
-const passwordInput = document.querySelector('.user-selected input')
-const passwordView = document.querySelector('.user-selected .passwordView')
+const home = document.querySelector('.home.container');
+const passwordInput = document.querySelector('.user-selected #password');
+const passwordView = document.querySelector('.user-selected .passwordView');
+const buttonLogin = document.querySelector(".home #logIn");
+
 const listsTask = document.querySelector('.listsTask.container')
 const lists = listsTask.querySelector('.lists-body .lists');
-const modelsArea = document.querySelector('.modelsArea');
+
 const tasksTamplete = document.querySelector('.tasks.container');
+
 const modalInput = document.querySelector('.modalInput');
+const modelsArea = document.querySelector('.modelsArea');
 
 const users = [
     {id:1,name:'Jordam Machael',password:'JM1',urlImg:'.../../media/homem.png'},
@@ -15,53 +19,147 @@ const users = [
     {id:4,name:'Nami Helena',password:'NH4',urlImg:'.../../media/mulher1.png'},
     {id:5,name:'Carolina Maria',password:'CM5',urlImg:'.../../media/mulher2.png'}
 ];
-const AllListTask = localStorage.getItem('allLists')?
+let AllListTask = localStorage.getItem('allLists')?
     JSON.parse(localStorage.getItem('allLists')):[];
 
 let userActive,listActive,typeToAdd;
 
-//Substituir Essa var com uma ligação ao Local Storge
+function goToHome(){
+    listsTask.style.display = 'none';
+    home.style.display = 'flex';
+}
 
-users.map(user=>{
-    const userOption = document.createElement('option');
-    userOption.setAttribute('value',user.id);
-    userOption.innerHTML = user.name;
-    select.append(userOption)
-})
+function mountUsersIntoSelect() {
+    users.map(user=>{
+        const userOption = document.createElement('option');
+        userOption.setAttribute('value',user.id);
+        userOption.innerHTML = user.name;
+    
+        select.append(userOption)
+    })
+}
 
-select.addEventListener('input',(e)=>{
-    const userId = Number(e.target.value)
-    const user = users.filter(user=>user.id === userId)[0];
+function enableUser(user){
+    userActive = user;
+    mountUserActive(userActive);
+}
 
-    enableUser(user)
-})
+function mountUserActive(user) {
+    home.querySelector('.user-img img').src = `${user.urlImg}`;
+    home.querySelector('.user--name').innerHTML = `${user.name}`;
 
-document.querySelector(".home #logIn").addEventListener('click',(e)=>{
-    e.preventDefault();
+    listsTask.querySelector('.user--img img').src = user.urlImg;
+    listsTask.querySelector('.user--name').innerHTML = user.name;
+}
 
-    if (userActive) {
-        const password = document.querySelector('.home #password').value;
-        confirmUser(userActive,password);
+//Validar a Entrada no password....
+function confirmUser(user,password) {
+    if(user.password === password){
+        goToList(user)
+        return;
     }
-});
+    showPassword();
+}
+//A partir do login ir na segundo pagina 'lists'
+function goToList(){
+    home.style.display = 'none';
+    tasksTamplete.style.display = 'none';
+    listsTask.style.display = 'block';
 
-passwordInput.addEventListener('input',()=>passwordView.style.opacity = '0')
+    updateList()
+}
 
-modalInput.querySelector('a')
-    .addEventListener('click',e=>{
-        e.preventDefault();
-        const itemText = getNewData().trim();
+function showPassword() {
+    passwordView.innerHTML = `A senha é: ${userActive.password}`;
+    passwordView.style.opacity = '1';
+}
 
-        if (itemText) {
-            if (typeToAdd === 'list') {
-                AllListTask.push(new CreateListTasks(itemText));
-                updateList();
-            } 
-            if(typeToAdd === 'tasks') {
-                listActive.tasks.push(new CreateTasks(itemText));
-                updateTasks();
-            }
+function updateList() {
+    localStorage.setItem('allLists',JSON.stringify(AllListTask));
+    assembleList(userActive.id);
+}
+
+//Carregar a coleção de tarefas do usuario Activo
+function assembleList(userId) {
+    lists.innerHTML = ``;
+    
+    AllListTask.filter(list=>list.userRef===userId)
+        .map(userList=>{
+            const listItem = modelsArea.querySelector('.list').cloneNode(true);
+
+            listItem.querySelector('.list--name').innerHTML = userList.title;
+            listItem.querySelector('.list--numberTask')
+                .innerHTML = userList.tasks.length;
             
-            hideModal();
-        }
+            listItem.addEventListener('click',(e)=>{
+                if(e.target.tagName==='BUTTON'){
+                    removeList(userList);
+                    return;
+                }
+                listActive = userList;
+                goToTasks(listActive)
+            });
+
+            lists.append(listItem);
+        });
+}
+
+function removeList(userList) {
+    AllListTask = AllListTask.filter(list=>list.id !== userList.id);
+    
+    updateList();
+}
+
+function goToTasks({title,tasks}) {
+    listsTask.style.display = 'none';
+    tasksTamplete.style.display = 'block';
+
+    tasksTamplete.querySelector('.tasks--name')
+        .innerHTML = title;
+
+    assembleTasks(tasks);
+}
+
+
+function assembleTasks(tasks) {
+    tasksTamplete.querySelector('.tasks-body').innerHTML='';
+    
+    tasks.map(task=>{
+        const taskItem = modelsArea.querySelector('.task').cloneNode(true);
+        
+        taskItem.classList.add(task.status?`task-done`:`task-to-do`)
+        
+        taskItem.querySelector('.task--name')
+            .innerHTML = task.name;
+        taskItem.querySelector('.task--status')
+            .innerHTML = task.status?`V`:`O`;
+        
+        taskItem.addEventListener('click',(e)=>{
+            /* Futuramente...
+            if(e.target.tagName==='BUTTON'){
+                removeTask(tasks,task)
+                return;
+            } 
+            */
+            
+            //trocar a tarefa de paraFazer à Feito e viceVersa
+            makeTaskDone(task)
+        });
+
+        tasksTamplete.querySelector('.tasks-body')
+            .append(taskItem);
     });
+}
+
+/*futuramente
+function removeTask(tasks,task) {
+    tasks.filter(({id})=> id !== task.id);
+
+    assembleTasks(listActive.tasks);
+} */
+
+function makeTaskDone(task) {
+    task.status = !task.status;
+
+    assembleTasks(listActive.tasks)
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,88 +1,50 @@
-//Selecionar e montar um user
-//Escolha de um usúario e  ative depois da escolha
-function enableUser(user){
-    userActive = user;
+mountUsersIntoSelect();
 
-    home.querySelector('.user-img img').src = `${userActive.urlImg}`;
-    home.querySelector('.user--name').innerHTML = `${userActive.name}`;
-}
+select.addEventListener('input',(e)=>{
+    const userId = Number(e.target.value)
+    const user = users.filter(user=>user.id === userId)[0];
 
-//Validar a Entrada no password....
-function confirmUser(user,password) {
-    if(passwordComfirmed(user, password)){
-        handleUserList(user)
-        return;
+    enableUser(user);
+})
+
+buttonLogin.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    if (userActive) {
+        const password = passwordInput.value;
+        confirmUser(userActive,password);
     }
-    showPassword();
-}
+});
 
-function passwordComfirmed(user,password) {
-    return user.password === password;
-}
+passwordInput.addEventListener('input',()=>passwordView.style.opacity = '0');
 
-//A partir do login ir na segundo pagina 'lists'
-function handleUserList(user) {
-    
-    home.style.display = 'none';
-    listsTask.style.display = 'block';
-    
-    listsTask.querySelector('.user--img img').src = user.urlImg;
-    listsTask.querySelector('.user--name').innerHTML = user.name;
-    
-    assembleList(user.id)
-}
-//Carregar enumeras tarefas apartir do usuario escolhido
 
-function assembleList(userId) {
-    lists.innerHTML = ``;
-    AllListTask.filter(list=>list.userRef===userId)
-        .map(userList=>{
-            const listItem = modelsArea.querySelector('.list').cloneNode(true);
+modalInput.querySelector('a')
+    .addEventListener('click',e=>{
+        e.preventDefault();
+        const itemText = getNewData().trim();
 
-            listItem.querySelector('.list--name').innerHTML = userList.title;
-            listItem.querySelector('.list--numberTask')
-                .innerHTML = userList.tasks.length;
+        if (itemText) {
+            if (typeToAdd === 'list') {
+                AllListTask.push(new CreateListTasks(itemText));
+                updateList();
+            } 
+            if(typeToAdd === 'tasks') {
+                listActive.tasks.push(new CreateTasks(itemText));
+                assembleTasks(listActive.tasks);
+            }
             
-            listItem.addEventListener('click',(e)=>{
-                listActive = userList;
-                handleTasks(listActive)
-            });
-
-            lists.append(listItem);
-        });
-
-}
-
-function showPassword() {
-    passwordView.style.opacity = '1';
-    passwordView.innerHTML = `A senha é: ${userActive.password}`;
-}
-
-function goToHome(){
-    listsTask.style.display = 'none';
-    home.style.display = 'flex';
-}
-function goToList(){
-    tasksTamplete.style.display = 'none';
-    listsTask.style.display = 'block';
-
-    updateList()
-}
-
-function goToTasks() {
-    listsTask.style.display = 'none';
-    tasksTamplete.style.display = 'block';
-
-    updateTasks();
-}
+            hideModal();
+        }
+    });
 
 function getNewData() {
     return modalInput.querySelector('input').value
 }
 
-function updateList() {
-    localStorage.setItem('allLists',JSON.stringify(AllListTask));
-    assembleList(userActive.id);
+function onAdd(typeItem) {
+    showModal();
+    typeToAdd = typeItem;
 }
 
 function showModal() {
@@ -93,51 +55,4 @@ function hideModal() {
     modalInput.querySelector('input').value = '';
 }
 
-//Da tarefa escolhida ter os feitos e não feiots
-function handleTasks({title,tasks}) {
-    goToTasks()
-
-    tasksTamplete.querySelector('.tasks--name')
-        .innerHTML = title;
-    
-    assembleTasks(tasks)
-}
-
-function assembleTasks(tasks) {
-    tasksTamplete.querySelector('.tasks-body').innerHTML='';
-    
-    tasks.map(task=>{
-        const taskItem = modelsArea.querySelector('.task').cloneNode(true);
-        const classAdicional = task.status?`task-done`:`task-to-do`;
-        
-        taskItem.classList.add(classAdicional)
-        
-        taskItem.querySelector('.task--name')
-            .innerHTML = task.name;
-        taskItem.querySelector('.task--status')
-            .innerHTML = task.status?`V`:`O`;
-        
-        taskItem.addEventListener('click',(e)=>{
-            makeTaskDone(task)
-            //trocar a tarefa de paraFazer à Feito e viceVersa
-        });
-
-        tasksTamplete.querySelector('.tasks-body')
-            .append(taskItem);
-    });
-}
-
-function makeTaskDone(task) {
-    task.status = !task.status;
-
-    updateTasks();
-}
-
-function updateTasks() {
-    assembleTasks(listActive.tasks);
-}
-
-function onAdd(typeItem) {
-    showModal();
-    typeToAdd = typeItem;
-}
+//melhorar a permanec
